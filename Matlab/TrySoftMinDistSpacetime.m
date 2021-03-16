7114aaeff3870c99be804fb0c02e8c3a28d89aac
Vismay <vismay@utexas.edu>
2021-03-16 13:52:08 -0500

    yixins new code

--- Matlab/TrySoftMinDistSpacetime.m
+++ Matlab/TrySoftMinDistSpacetime.m
@@ -2,7 +2,8 @@
 addpath("../external/smooth-distances/build/");
 %fname = "../Scenes/output_results/eight_agents/agent_circle/";
 %fname = "../Scenes/output_results/three_agents/test/";
-fname = "../Scenes/output_results/scaling_tests/test/";
+%fname = "../Scenes/output_results/scaling_tests/test/";
+fname = "../Scenes/output_results/scaling_tests/10_agents/";
 
 setup_params = jsondecode(fileread(fname+"setup.json"));
 
@@ -32,6 +33,7 @@ AdjM = adjacency_matrix(scene.terrain.F);
 AdjM_visited = AdjM;
 
 a = fieldnames(setup_params.agents);
+
 for i = 1:numel(a)
     agent.id = i;
     agent.xse = getfield(setup_params.agents, a{i}).xse;
@@ -57,23 +59,28 @@ for i = 1:numel(a)
     disp(P1);
     % visualization
     vv = VV(P1,:);
+    vv(:,3) = linspace(0,10, size(vv,1));
     r1e = [(1:agent.segments)' (2:(agent.segments+1))'];
+    r1v = interp1(vv(:,3), vv(:,1:2), linspace(agent.xse(1,3),agent.xse(end,3),agent.segments+1));
+    r1v = [r1v linspace(agent.xse(1,3),agent.xse(end,3),agent.segments+1)'];
     
     % edges
     agent.e = r1e;
     r1e = r1e + size(v,1);
+    
     e = [e; r1e];
     
     % vertices
-    agent.v = vv;            
-    v = [v;vv];
-   
-    disp(i);
+    endtime = r1v(end,3);
+    r1v(:,3) = sort(rand(1,size(r1v,1))*(endtime));%r1v(:,3)/i;%
+    r1v(end,3) = endtime;
+    agent.v = r1v;            
+    v = [v;r1v];
 end
 
 PV = v;
 PE = e;
-[CV,CF,CJ,CI] = edge_cylinders(PV,PE, 'Thickness',1, 'PolySize', 10);
+[CV,CF,CJ,CI] = edge_cylinders(PV,PE, 'Thickness',0.1, 'PolySize', 10);
 figure;
 surf_anim = tsurf(CF, CV); 
 axis equal;
