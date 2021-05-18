e3025f48b852897914716d4b3b6898eac33da5f3
chenyixin1008 <chenyixin1008@gmail.com>
2021-05-18 16:44:38 -0400

    regularizer_hessian

--- Matlab/hessfcn.m
+++ Matlab/hessfcn.m
@@ -11,12 +11,14 @@ function [H] = hessfcn(q_i,lambda)
     K_map =   0*scene.coeff_matrix(4,:);
     K_ke =    1*scene.coeff_matrix(5,:);
     K_pv =    0*scene.coeff_matrix(6,:);
+    K_reg =   1;
     
-    [e, H_ke] = kinetic_energy_with_hessian(Q, scene, K_ke);
+    [e_ke, H_ke] = kinetic_energy_with_hessian(Q, scene, K_ke);
+    [e_reg, H_reg] = regularizer_energy_with_hessian(Q, scene, K_reg);
     
     H = zeros(size(q_i,1),size(q_i,1));
     for ii=1:num_agents
-        H(1+(ii-1)*size(Q,1):ii*size(Q,1),1+(ii-1)*size(Q,1):ii*size(Q,1)) = H_ke(:,:,ii);
+        H(1+(ii-1)*size(Q,1):ii*size(Q,1),1+(ii-1)*size(Q,1):ii*size(Q,1)) = H_ke(:,:,ii) + H_reg(:,:,ii);
         %H_full(end-ii+1,end-ii+1) = 1; %assign the last block to identity
     end
     
@@ -56,6 +58,40 @@ function [e, HT] = kinetic_energy_with_hessian(Q, scene, K)
         end
                
         HT(:,:,i) = K(i)*d2Edq2;
+            
+    end
+end
+
+function [e, HT] = regularizer_energy_with_hessian(Q, scene, K)
+    if sum(K)==0
+        e=0;
+        HT = zeros(size(Q,1), size(Q,1), size(Q,2));
+        return;
+    end
+    HT = zeros(size(Q,1), size(Q,1), size(Q,2));
+    e=0;
+    for i=1:numel(scene.agents)
+        q_i = Q(:, i); %3*nodes
+        dx = reshape(q_i(4:end) - q_i(1:end -3), 3, numel(q_i)/3-1)';
+        dt = dx(:,3)+1e-6;%add epsilon to make sure there is never a divide by 0
+        endtime = q_i(end);
+        segments = numel(q_i)/3-1;
+        kt = (endtime/segments)*ones(size(dx,1),1);%regular time intervals over the rod;
+        e = e + K*(sum(kt./dt));
+       
+        d2Edq2 = zeros(numel(q_i),numel(q_i));
+        
+        for kk=1:numel(q_i)/3-1
+            d2Edq2((kk-1)*3+3,(kk-1)*3+3) = d2Edq2((kk-1)*3+3,(kk-1)*3+3) + 2*kt(kk,1)./(dt(kk,1).^3);
+
+            d2Edq2((kk-1)*3+3,(kk-1)*3+6) = d2Edq2((kk-1)*3+3,(kk-1)*3+6) - 2*kt(kk,1)./(dt(kk,1).^3);
+
+            d2Edq2((kk-1)*3+6,(kk-1)*3+3) = d2Edq2((kk-1)*3+6,(kk-1)*3+3) - 2*kt(kk,1)./(dt(kk,1).^3);
+
+            d2Edq2((kk-1)*3+6,(kk-1)*3+6) = d2Edq2((kk-1)*3+6,(kk-1)*3+6) + 2*kt(kk,1)./(dt(kk,1).^3);
+        end
+               
+        HT(:,:,i) = K*d2Edq2;
             
     end
 end
